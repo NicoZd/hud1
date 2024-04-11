bf3f09247b1818c6a98a6efec99d5bb3a9e95c94
NicoZd <166566522+NicoZd@users.noreply.github.com>
2024-04-11 21:01:16 +0200

    Add Active Property and Binding

--- Hud1/Hud1/MainWindow.xaml.cs
+++ Hud1/Hud1/MainWindow.xaml.cs
@@ -1,48 +1,19 @@
-﻿using System.ComponentModel;
+﻿using Hud1.model;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Hud1
 {
-    public partial class MainWindow: INotifyPropertyChanged
-
+    public partial class MainWindow: Window
     {
-        public double _ddd;
-        public double ddd
-        {
-            get {
-                return _ddd;
-            }
-            
-            set {
-                _ddd = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
+        WindowModel windowModel = new WindowModel();
 
         public MainWindow()
         {
-            InitializeComponent();
-            this.DataContext = this;
-        }
-
-        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            this.DataContext = windowModel;
         }
 
         protected override void OnSourceInitialized(EventArgs e)
@@ -51,10 +22,7 @@ namespace Hud1
             var hwnd = new WindowInteropHelper(this).Handle;
             Debug.WriteLine("OnSourceInitialized {0}", hwnd);
             WindowsServices.SetWindowExTransparent(hwnd);
-        }
 
-        private void OnWindowLoaded(object sender, RoutedEventArgs e)
-        {
             int width = (int)System.Windows.SystemParameters.PrimaryScreenWidth;
             int height = (int)System.Windows.SystemParameters.PrimaryScreenHeight;
 
@@ -66,24 +34,15 @@ namespace Hud1
             this.Top = 0;
         }
 
-        private void OnWindowGotFocus(object sender, RoutedEventArgs e)
-        {
-            Debug.WriteLine("OnWindowGotFocus");
-        }
-
-        private void OnWindowLostFocus(object sender, RoutedEventArgs e)
-        {
-            Debug.WriteLine("OnWindowLostFocus");
-        }
         private void OnWindowActivated(object sender, EventArgs e)
         {
             Debug.WriteLine("OnWindowActivated");
-            ddd = 4;
+            windowModel.active = true;
         }
         private void OnWindowDeactivated(object sender, EventArgs e)
         {
             Debug.WriteLine("OnWindowDeactivated");
-            ddd = 1;
+            windowModel.active = false;
         }
 
     }
---
+++ Hud1/Hud1/converters/ActiveToVisibility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Diagnostics;
+
+namespace Hud1.converters
+{
+    [ValueConversion(typeof(Boolean), typeof(Visibility))]
+    class ActiveToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Boolean active = (Boolean)value;
+            Debug.WriteLine("ActiveToVisibility {0}", active);
+            return active ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+        
+    }
+}
