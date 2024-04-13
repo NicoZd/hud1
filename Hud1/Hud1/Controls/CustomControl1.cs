29bf5dc06422508d687431c2fd832c4eb6dc84fd
NicoZd <166566522+NicoZd@users.noreply.github.com>
2024-04-13 19:59:22 +0200

    Add custom property

--- Hud1/Hud1/Controls/CustomControl1.cs
+++ Hud1/Hud1/Controls/CustomControl1.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,9 +21,54 @@ namespace Hud1.Controls
 {   
     public class CustomControl1 : Control
     {
+        public Label LabelControl;
+
+        public static DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(String), typeof(CustomControl1), new PropertyMetadata("Default", new PropertyChangedCallback(CustomTextBox_OnTextPropertyChanged)));
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
         static CustomControl1()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
+        }
+
+        private static void CustomTextBox_OnTextPropertyChanged(DependencyObject d,
+                    DependencyPropertyChangedEventArgs e)
+        {
+            CustomControl1 customTextBox = d as CustomControl1;
+
+            Debug.Print("xxx {0}", e.NewValue);
+
+            customTextBox.Test();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            LabelControl = Template.FindName("PART_Label", this) as Label;
+
+            //Binding labelBinding = new Binding
+            //{
+            //    Path = new PropertyPath(nameof(Label)),
+            //    Source = this
+            //};
+
+            //LabelControl.SetBinding(ContentControl.ContentProperty, labelBinding);
+
+            LabelControl.Content = Label;
+        }
+
+        public void Test()
+        {            
+            if (LabelControl != null)
+            {
+                LabelControl.Content = Label;
+            }
         }
     }
 }
