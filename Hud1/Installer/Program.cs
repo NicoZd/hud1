21268beb012b4ec12c2f935f43588094270d6325
NicoZd <166566522+NicoZd@users.noreply.github.com>
2024-05-09 22:50:38 +0200

    Add installer project

---
+++ Hud1/Installer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+var exeFolder = Path.GetDirectoryName(Process.GetCurrentProcess()!.MainModule!.FileName);
+Console.WriteLine("Working Dir {0}", exeFolder);
+void Run(ProcessStartInfo info)
+{
+    var process = new Process { StartInfo = info };
+    process.Start();
+    process.WaitForExit();
+}
+
+var clean = new ProcessStartInfo();
+clean.FileName = "dotnet";
+clean.Arguments = "clean ../../../../Hud1/Hud1.csproj";
+
+var publish = new ProcessStartInfo();
+publish.FileName = "dotnet";
+publish.Arguments = "publish -p:PublishProfile=Hud1/Properties/PublishProfiles/FastProfile ../../../../Hud1/Hud1.csproj";
+
+var inno = new ProcessStartInfo();
+inno.FileName = "C:/Program Files (x86)/Inno Setup 6/ISCC.exe";
+inno.Arguments = "../../../app.installer.iss";
+
+var binDir = "../../../../Hud1/bin";
+Run(clean);
+if (Directory.Exists(binDir))
+{
+    Console.WriteLine("Delete Folder");
+    Directory.Delete(binDir, true);
+}
+Run(publish);
+Run(inno);
+
+Process.Start("explorer.exe", "C:\\Workspaces\\nico_2024\\hud1\\Hud1\\Hud1\\bin\\Installer");
