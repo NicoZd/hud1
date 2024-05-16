5f67da8a43ddc0186e8b09b9e6667c7c16fa1c68
NicoZd <166566522+NicoZd@users.noreply.github.com>
2024-05-16 19:44:23 +0200

    Rename to BringIntoView

---
+++ Hud1/Hud1/Views/BringIntoView.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace Hud1.Views
+{
+    public class BringIntoView : UIElement
+    {
+        public static readonly DependencyProperty BringIntoViewProperty =
+            DependencyProperty.RegisterAttached(
+                "BringIntoView",
+                typeof(bool),
+                typeof(BringIntoView),
+                new FrameworkPropertyMetadata(defaultValue: false,
+                    flags: FrameworkPropertyMetadataOptions.AffectsRender,
+                    OnChange
+                )
+        );
+
+        private static void OnChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue == true)
+            {
+                ((FrameworkElement)d).BringIntoView(new Rect(0, -100, 0, 200));
+            }
+        }
+
+        public static bool GetBringIntoView(UIElement target) =>
+            (bool)target.GetValue(BringIntoViewProperty);
+
+        public static void SetBringIntoView(UIElement target, bool value) =>
+            target.SetValue(BringIntoViewProperty, value);
+    }
+}
