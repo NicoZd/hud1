8731af65acc2b97e268d9876235f56b7abf17d53
NicoZd <166566522+NicoZd@users.noreply.github.com>
2024-06-06 16:03:23 +0200

    Design and text tweaks

--- Hud1/Hud1/Models/NavigationStates.cs
+++ Hud1/Hud1/Models/NavigationStates.cs
@@ -5,16 +5,16 @@ internal class NavigationStates
     internal static readonly NavigationState ALL = new();
 
     // MENU
-    internal static readonly NavigationState MENU_NIGHTVISION = new() { Hint = "Use Alt+Shift+N anytime to toogle Nightvision. Adjust Gamma Settings to select intensity." };
+    internal static readonly NavigationState MENU_NIGHTVISION = new() { Hint = "Press Alt+Shift+N anytime to toggle Nightvision. Adjust Gamma to configure intensity." };
     internal static readonly NavigationState NIGHTVISION_VISIBLE = new();
 
-    internal static readonly NavigationState MENU_MACRO = new() { Hint = "Start Mouse & Keyboard Macros." };
+    internal static readonly NavigationState MENU_MACRO = new() { Hint = "Start Mouse & Keyboard Macros. Open Folder to edit and add new Macros." };
     internal static readonly NavigationState MACRO_VISIBLE = new();
 
-    internal static readonly NavigationState MENU_CROSSHAIR = new() { Hint = "Toggle (Alt+Shift+C) and configure Crosshair." };
+    internal static readonly NavigationState MENU_CROSSHAIR = new() { Hint = "Press Alt+Shift+C anytime to toggle crosshair. Ajdust Form, Color, Size here." };
     internal static readonly NavigationState CROSSHAIR_VISIBLE = new();
 
-    internal static readonly NavigationState MENU_MORE = new() { Hint = "Quit, Skin, Adjust, Info" };
+    internal static readonly NavigationState MENU_MORE = new() { Hint = "Quit Application, Configure HUD and Style." };
     internal static readonly NavigationState MORE_VISIBLE = new();
 
     // NIGHTVISION
@@ -35,11 +35,11 @@ internal class NavigationStates
     internal static readonly NavigationState MACROS_FOLDER = new() { Label = "Open Folder", Hint = "Open explorer.exe in Macros Folder." };
 
     // MORE
-    internal static readonly NavigationState EXIT = new() { Label = "Exit", Hint = "Exit HUD and terminate process." };
+    internal static readonly NavigationState EXIT = new() { Label = "Exit", Hint = "Exit HUD and terminate process. Alt + F4 will do when window is active." };
 
     internal static readonly NavigationState HUD_POSITION = new() { Label = "HUD Position", Hint = "Select Position of the HUD" };
     internal static readonly NavigationState TOUCH_MODE = new() { Label = "Touch Mode", Hint = "If enabled this window will not be activated on mouse or touch interaction.", SelectionLeftLabel = "< Off", SelectionRightLabel = "On >" };
 
     internal static readonly NavigationState STYLE = new() { SelectionLabel = "Green", Label = "Colors", Hint = "Adjust colors of this window." };
-    internal static readonly NavigationState FONT = new() { SelectionLabel = "Fira Code", Label = "Font", Hint = "Select Font" };
+    internal static readonly NavigationState FONT = new() { SelectionLabel = "Fira Code", Label = "Font", Hint = "Select Font, open UserData folder (see macros) to add or remove fonts." };
 }
